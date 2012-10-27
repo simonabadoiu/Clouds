58eee9a11a9e381c02246c9f0c40a7785720865e
Simona Andreea Badoiu <simonaandreea.badoiu@gmail.com>
2012-10-28 00:58:42 +0300

    Main Menu created

--- src/com/example/clouds/StartMenu.java
+++ src/com/example/clouds/StartMenu.java
@@ -1,14 +1,15 @@
-package com.example.main;
+package com.example.clouds;
 
-import com.example.clouds.Activity1;
 import com.example.clouds.R;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class StartMenu extends Activity{
 
@@ -16,23 +17,27 @@ public class StartMenu extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
 	   super.onCreate(savedInstanceState);
 	   this.setContentView(R.layout.start_menu);
-	   
+	   LinearLayout ll0 = (LinearLayout)findViewById(R.id.newTHbtn1);
 	   Button create = new Button(this);
+	   create.setText("Create Quest");
+	   ll0.addView(create);
 	   create.setOnClickListener(new OnClickListener() {
 
     	   @Override
     	   public void onClick(View v) {
     	   }
 	   });
-	   
+	   LinearLayout ll1 = (LinearLayout)findViewById(R.id.playTHbtn);
 	   Button play = new Button(this);
+	   play.setText("Play Quest");
+	   ll1.addView(play);
 	   play.setOnClickListener(new OnClickListener() {
 
     	   @Override
     	   public void onClick(View v) {	   
     		   Intent myIntent = new Intent(StartMenu.this, Activity1.class);
-
-	    	   startActivity(myIntent);
+	    	   Log.d("MSG", "Deschide nou activity");
+    		   startActivity(myIntent);
     	   }
 	   });
 	}	
